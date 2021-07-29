af59e82f315ad7453422ab85877ed73d82fbc8d4
JeanWhisky <john@lizeprod.de>
2021-07-29 23:21:57 +0200

    Upload Cookie Stream

---
+++ Web/Cookie Stream/README.md
@@ -0,0 +1,28 @@
+![ImaginaryCTF](../../banner.png)
+
+# Cookie Stream
+
+|Author|Points|Category|Solves|
+|---|---|---|---|
+|Eth007|150|Web|86|
+
+### Description
+
+```
+Cookie streaming service? Naaaaaaah. Password protected Rickroll as a Service? YAAAAAAAAAAAAAAAAAAAAAAAAAAAASSSSSSSSSSS!	
+```
+
+### Attachments
+
+```
+* [app.py](app.py)
+* http://cookie-stream.chal.imaginaryctf.org/
+```
+
+
+
+
+There is our flag:
+```
+ictf{d0nt_r3us3_k3ystr34ms_b72bfd21}
+```
---
+++ Web/Cookie Stream/solver.py
@@ -0,0 +1,27 @@
+from binascii import hexlify, unhexlify
+from Crypto.Util.Padding import pad, unpad
+
+orig_auth = '895462eacc651da4dd302f43d42eaccaffab31053df1cb60'
+
+nonce = '895462eacc651da4' #First 16Bytes
+ciphertext1 = "dd302f43d42eaccaffab31053df1cb60" #Last 32Bytes
+plaintext1 = "Eth007"
+plaintext2 = "admin"
+
+
+
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+plaintext1 = (pad(plaintext1.encode(),16))
+plaintext2 = (pad(plaintext2.encode(),16))
+
+p1XORp2 = byte_xor(plaintext1, plaintext2)
+
+ciphertext1 = unhexlify(ciphertext1)
+
+c1XOR_p1XORp2_ = byte_xor(ciphertext1, p1XORp2)
+
+print(" New Auth-Cookie: " + nonce + hexlify(c1XOR_p1XORp2_).decode())
+
+### New Auth-Cookie: 895462eacc651da4f9202a1a8a12adcbfeaa30043cf0ca61
